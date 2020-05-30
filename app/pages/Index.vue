2a02f8cfe688e01a36c88b4243be15e81f26ca4c
Kalogon <ghlee5084@kaist.ac.kr>
2020-05-31 01:48:48 +0900

    Add Tag Component

--- app/pages/Index.vue
+++ app/pages/Index.vue
@@ -1,4 +1,28 @@
 <template>
 	<main class="Index">
+		<ul>
+			<li v-for="tag_name in tag_names" :key="tag_name">
+				<Tag :name="tag_name"></Tag>
+			</li>
+		</ul>
+		
 	</main>
 </template>
+
+<style scoped>
+</style>
+
+<script>
+	import Tag from "@/components/Tag"
+
+	export default {
+		data(){
+			return {
+				tag_names: ["skinny_leg", "small_face", "long_leg"]
+			}
+		},
+		components: {
+			Tag
+		}
+	}
+</script>
