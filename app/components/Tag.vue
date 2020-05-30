318f0b694b0d864ad083560bafeb342f77150631
Kalogon <ghlee5084@kaist.ac.kr>
2020-05-31 01:48:48 +0900

    Add Tag Component

---
+++ app/components/Tag.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        {{nameWithHash}}
+    </div>
+</template>
+
+<style scoped>
+    div{
+        font-family: 'Raleway';
+        width: 140px;
+        height: 25px;
+        background-color: #C1DFFB;
+        border-radius: 50px;
+        font-size: medium;
+        text-align: center;
+        line-height: 25px;
+    }
+</style>
+
+<script>
+    export default {
+        computed: {
+            nameWithHash(){
+                return "#" + this.name
+            }
+        },
+
+        props: {
+            name: {
+                type: String,
+                default: "None"
+            }
+        }
+    }
+</script>
