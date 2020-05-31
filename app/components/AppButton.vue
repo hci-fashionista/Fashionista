a78b65dd3fe70682177bbe6d42aef60aaaaaef14
Khinenw <deu07115@gmail.com>
2020-05-31 16:03:11 +0900

    Add button component

---
+++ app/components/AppButton.vue
@@ -0,0 +1,32 @@
+<template>
+	<button @click="onClick">
+		<slot></slot>
+	</button>
+</template>
+
+<style scoped>
+	button {
+		cursor: pointer;
+		color: #ffffff;
+		font-family: 'Raleway', sans-serif;
+		background: #2196f3;
+		border: none;
+		border-radius: 5px;
+		padding: 10px 20px;
+		transition: all .4s ease;
+
+		&:hover {
+			background: #42a5f5;
+		}
+	}
+</style>
+
+<script>
+	export default {
+		methods: {
+			onClick() {
+				this.$emit('click');
+			}
+		}
+	};
+</script>
