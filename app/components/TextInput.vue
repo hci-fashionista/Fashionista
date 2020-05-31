878aa7054315daf73327943e45929667d688b859
panya <kjy2844@kaist.ac.kr>
2020-05-31 16:24:11 +0900

    Add TextInput

---
+++ app/components/TextInput.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<input type="text" v-model="typetext">
+		{{typetext}}
+	</div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+	export default {
+		data() {
+			return {
+				typetext: ""
+			};
+		},
+
+		model: {
+			prop: "message",
+			event: "textSubmit"
+		},
+
+		props: {
+			message: {
+				type: String,
+				default : 'text'
+			}
+		},
+
+		watch: {
+			typetext () {
+				this.$emit('textSubmit', this.typetext);
+			}
+		}
+	};
+</script>
