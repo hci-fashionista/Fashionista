a7c1197c825c4bcd6f16f8228376f9a98ca284ef
Kalogon <ghlee5084@kaist.ac.kr>
2020-06-01 16:51:33 +0900

    Add Cloth Component

---
+++ app/components/AppCloth.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="cloth">
+        <div class="clothImage"><img :src="Tshirts" alt="cloth image" width="228" height="240"></div>
+        <div class="clothName">{{this.name}}</div>
+        <div class="description">
+            <div class="price">{{priceWithWon}}</div>
+            <div class="like"><AppLike :likes="likes"></AppLike></div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+    div.cloth{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    div.clothName{
+        font-family: 'Roboto';
+        width: 100%;
+        font-size: 24px;
+        margin-left: 5px;
+    }
+    div.description{
+        display: flex;
+        width: 100%;
+    }
+    div.price {
+        font-size: 18px;
+        flex-grow: 1;
+        margin-left: 5px;
+    }
+    div.like{
+        margin-right: 5px;
+    }
+</style>
+
+<script>
+    import AppLike from "@/components/AppLike";
+    import Tshirts from '@/images/Tshirts.png';
+
+    export default {
+        data(){
+            return {
+                Tshirts
+            }
+        },
+        computed: {
+            priceWithWon(){
+                return "￦" + this.price
+            }
+        },
+        props: {
+            name: {
+                type: String,
+                default: "None"
+            },
+            price: {
+                type: String,
+                default: "0"
+            },
+            likes: {
+                type: Number,
+                default: 0
+            }
+        },
+        components: {
+            AppLike
+        }
+    }
+</script>
---
+++ app/components/AppClothwithRank.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="clothwithrank">
+        <div class="info">
+            <div class="rank">{{detail.rank}}</div>
+            <div class="new" v-if="detail.is_new">new</div>
+        </div>
+        <AppCloth :name="detail.name" :price="detail.price" :likes="detail.likes"></AppCloth>
+    </div>
+</template>
+
+<style scoped>
+    div.clothwithrank{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    div.info{
+        height: 100%;
+        align-self: start;
+    }
+
+    div.new{
+        width: 56px;
+        height: 56px;
+        border-radius: 30px;
+        background-color: #F48484;
+        text-align: center;
+        color: white;
+        line-height: 56px;
+        font-family: 'Raleway';
+        font-size: 20px;
+        font-weight: 800;
+    }
+
+    div.rank{
+        width: 56px;
+        height: 56px;
+        margin-bottom: 5px;
+        background-color: var(--blue-400);
+        text-align: center;
+        color: white;
+        line-height: 56px;
+        font-family: 'Roboto';
+        font-size: 41px;
+        font-weight: 800;
+    }
+</style>
+
+<script>
+    import AppCloth from "@/components/AppCloth";
+    export default {
+        props: {
+            detail: {
+                type: Object
+            }
+        },
+        components: {
+			AppCloth
+		}
+    }
+</script>
