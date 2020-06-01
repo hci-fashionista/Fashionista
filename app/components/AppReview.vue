14c3e75f3a395be0eeeba9c6b8badeb0f1cfcbda
Khinenw <deu07115@gmail.com>
2020-06-01 18:33:18 +0900

    Unify indentation, line ending

--- app/components/AppReview.vue
+++ app/components/AppReview.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="review">
-        <span id="ID"><strong>
-            {{id}}
-        </strong></span>
-        <span id="content">
-            {{content}}
-        </span>
-    </div>
+	<div class="review">
+		<span id="ID"><strong>
+			{{id}}
+		</strong></span>
+		<span id="content">
+			{{content}}
+		</span>
+	</div>
 </template>
 
 <style scoped>
-    .review {
-        background: var(--grey-750);
-        border-radius: 18px;
+	.review {
+		background: var(--grey-750);
+		border-radius: 18px;
 
-        padding : 3px 12px;
-    }
+		padding : 3px 12px;
+	}
 
-    #id, #content {
-        font-family: Titillium Web;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 13.7647px;
-        line-height: 21px;
+	#id, #content {
+		font-family: Titillium Web;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 13.7647px;
+		line-height: 21px;
 
-    }
+	}
 </style>
 
 <script>
-    export default {
+	export default {
 
-        props: {
-            id: {
-                type: String,
-                default: "None"
-            },
-            content : {
-                type: String,
-                default: "None"
-            }
-        }
-    }
+		props: {
+			id: {
+				type: String,
+				default: "None"
+			},
+			content : {
+				type: String,
+				default: "None"
+			}
+		}
+	}
 </script>
--- app/components/AppTag.vue
+++ app/components/AppTag.vue
@@ -1,35 +1,35 @@
 <template>
-    <div>
-        {{nameWithHash}}
-    </div>
+	<div>
+		{{nameWithHash}}
+	</div>
 </template>
 
 <style scoped>
-    div{
-        font-family: 'Raleway';
-        width: 140px;
-        height: 25px;
-        background-color: var(--blue-800);
-        border-radius: 50px;
-        font-size: medium;
-        text-align: center;
-        line-height: 25px;
-    }
+	div{
+		font-family: 'Raleway';
+		width: 140px;
+		height: 25px;
+		background-color: var(--blue-800);
+		border-radius: 50px;
+		font-size: medium;
+		text-align: center;
+		line-height: 25px;
+	}
 </style>
 
 <script>
-    export default {
-        computed: {
-            nameWithHash(){
-                return "#" + this.name
-            }
-        },
+	export default {
+		computed: {
+			nameWithHash(){
+				return "#" + this.name
+			}
+		},
 
-        props: {
-            name: {
-                type: String,
-                default: "None"
-            }
-        }
-    }
+		props: {
+			name: {
+				type: String,
+				default: "None"
+			}
+		}
+	}
 </script>
