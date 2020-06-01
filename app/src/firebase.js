26f2ae36db8dca835868e82092cd2cc9e05b05d0
Khinenw <deu07115@gmail.com>
2020-06-01 17:01:05 +0900

    Add firebase

---
+++ app/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyB-5VFYc50HeSc6X64XzfxyEzxpXqe3Nog",
+	authDomain: "fashionista-cb19d.firebaseapp.com",
+	databaseURL: "https://fashionista-cb19d.firebaseio.com",
+	projectId: "fashionista-cb19d",
+	storageBucket: "fashionista-cb19d.appspot.com",
+	messagingSenderId: "389979579690",
+	appId: "1:389979579690:web:08af5fe4b9d0f56c85aa53",
+	measurementId: "G-KGNY0T9YEV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
