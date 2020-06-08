477233e5141677bb86537bcee87167c25653eef7
pacokwon <haechank@gmail.com>
2020-06-09 01:17:55 +0900

    update: organized Coordination Page styles

--- app/pages/MyCoordinations.vue
+++ app/pages/MyCoordinations.vue
@@ -18,88 +18,106 @@
 </template>
 
 <style scoped>
-    .container{
-        display: flex;
-        flex-direction: column;
-    }
+	.flex {
+		display: flex;
 
-	.clickable {
-		cursor: pointer;
-	}
-
-    .rankings{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .rankings > h1 {
-        margin-left: 20px;
-        align-self: start;
-    }
-    .coordinations_list {
-        list-style: none;
-        padding: 0px 0px;
-        display: grid;
-        grid-gap: 10px;
-        grid-auto-rows: minmax(100px, auto);
-		justify-items: center;
-	}
-
-	.coordinations_list > .plus_button {
-		margin: 20px 20px;
-
-		& > p{
-			font-size: 30px;
-			margin: 10px;
-			text-align: center;
+		&.center {
+			justify-content: center;
 		}
 
-		& > button {
-			font-size: 100px;
-			width: 160px;
-			height: 160px;
+		&.container{
+			width: 80vw;
+			flex-direction: column;
+			align-items: flex-start;
+		}
+	}
+
+	.coordinations {
+		max-width: 1500px;
+		flex-direction: column;
+
+		& > h1 {
+			margin-bottom: 0;
+			align-self: start;
+			font-size: 2.4rem;
+		}
+
+		& li {
 			cursor: pointer;
 		}
 	}
 
-    .coordinations_list > li {
-        margin: 20px 20px;
-    }
+	.flex.coordinations > ul {
+		list-style: none;
+		padding: 0px 0px;
+		justify-items: center;
+		display: grid;
 		grid-template-columns: repeat(4, 1fr);
+		grid-gap: 10px;
+		grid-auto-rows: minmax(100px, auto);
+		width: 100%;
+
+		& > .create {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			margin: 20px 20px;
+
+			& > p {
+				font-size: 20px;
+				margin: 10px;
+				text-align: center;
+			}
+
+			& > button {
+				width: 160px;
+				height: 160px;
+				border: none;
+				border-radius: 10px;
+				cursor: pointer;
+				outline: none;
+				font-size: 100px;
+				background-color: var(--grey-750)
+			}
+		}
+
+		& > li {
+			margin: 20px 20px;
+		}
+	}
 </style>
 
 <script>
-    import AppClothwithRank from "@/components/AppClothwithRank"
-    import AppTag from "@/components/AppTag"
+	import AppClothwithRank from "@/components/AppClothwithRank"
+	import AppTag from "@/components/AppTag"
 	import TagInput from "@/components/TagInput"
-    import firebase from "../src/firebase.js"
+	import firebase from "../src/firebase.js"
 	import CoordinationwithRank from "@/components/CoordinationwithRank"
 	import UploadRanking from "@/components/UploadRanking"
 	import IconPlus from "@/images/IconPlus.svg?inline";
 
-    const db = firebase.firestore()
+	const db = firebase.firestore()
 
-    export default {
-        data() {
-            return {
-                clothIds: [1,2,3,4,5,6,7,8,9],
-                total_coordinations: [],
-                my_coordinations: [],
-                height: "150~160",
-                weight: "50~60",
+	export default {
+		data() {
+			return {
+				clothIds: [1,2,3,4,5,6,7,8,9],
+				total_coordinations: [],
+				my_coordinations: [],
+				height: "150~160",
+				weight: "50~60",
 				clothes_dict: {},
 				selected_info: null
-            }
-        },
-        methods: {
-            uploadRankingPopupOpen(coordi) {
+			}
+		},
+		methods: {
+			uploadRankingPopupOpen(coordi) {
 				this.selected_info = coordi;
 				this.$nextTick(() => {
 					if(this.$refs.uploadRankingPopup)
 						this.$refs.uploadRankingPopup.open();
 				});
 			},
-
 			filtering(coordination){
 				if (coordination.author == "Dol Lee") {
 					return true;
